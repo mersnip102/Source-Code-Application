a9515b0f81e784b12335f2caef0df1b43bc450f3
Thuyttgch190424 <89965253+Thuyttgch190424@users.noreply.github.com>
2022-03-10 11:48:18 +0700

    update

--- Customer/customer.js
+++ Customer/customer.js
@@ -98,44 +98,44 @@ router.get('/proDetail', async (req, res) => {
 
 router.get('/feedback', async (req, res) => {
 
-    const id = req.query.id
+    const id = req.query.fb
+    console.log(id)
+    const dbo = await getDatabase();
     
-    const category = await categories()
-    
-    res.render('feedbackUser', {category: category, totalProduct:totalProduct, id:id})
+    const order = await dbo.collection('Order').findOne({_id: ObjectId(id)});
+    const email = order.email
+    res.render('feedbackUser', {totalProduct:totalProduct, email: email, idOrder: order.id})
 
 })
 
 router.post('/feedback', async (req, res) => {
     
     const fb = req.body.feedback
-    const id = req.body.id
+    var id = req.body.id
+    const email = req.body.email
     console.log(id)
     var d = new Date();
     const date = [d.getDate(), d.getMonth() + 1, d.getFullYear()].join('/') + ' ' + [d.getHours(),d.getMinutes(),d.getSeconds()].join(':');
     const dbo = await getDatabase();
     
-    const order = await dbo.collection('Order').findOne({_id: ObjectId(id)});
+    // const order = await dbo.collection('Order').findOne({_id: ObjectId(id)});
+    // console.log(order)
+    const user = await dbo.collection('Customer').findOne({email: email});
     
-    const user = await dbo.collection('Customer').findOne({email: order.email});
     
-    
-
     const collectionName = 'Feedback' 
     const newOrder = {
         nameCustomer: user.fullName,
         date: date,
         feedback: fb,
-        emailCustomer: order.email,
-        idOrder: order._id.toHexString(),
-        phoneNumber: order.orderDetail.phoneNumber
-        
+        emailCustomer: email,
+        idOrder: id.toHexString()
     }
     await insertObjectToCollection(collectionName, newOrder)
 
     // const category = await categories()
     var status = 'Feedback/Cancel order successful'
-    res.redirect('/user/purchaseHistory?email='+order.email)
+    res.redirect('/user/purchaseHistory?email='+email)
 
 })
 
